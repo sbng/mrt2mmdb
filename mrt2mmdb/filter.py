f86a756bf84f2e97a17a12f397ddba56ec990838
sbng <seo.boon.ng@gmail.com>
2024-09-10 16:39:17 +0800

    Refractor the code such that the data are written to the mmdb file within the loop to save memory utilization

--- mrt2mmdb/filter.py
+++ mrt2mmdb/filter.py
@@ -16,7 +16,19 @@ from maxminddb.errors import InvalidDatabaseError
 from maxminddb.file import FileBuffer
 from maxminddb.types import Record
 from os import PathLike
-from typing import Any, AnyStr, IO, Optional, Tuple, Union, cast, Dict, List, Tuple, Union
+from typing import (
+    Any,
+    AnyStr,
+    IO,
+    Optional,
+    Tuple,
+    Union,
+    cast,
+    Dict,
+    List,
+    Tuple,
+    Union,
+)
 
 from mmdb_writer import Encoder
 from maxminddb.reader import Reader
@@ -35,139 +47,59 @@ from args import (
 # pylint: disable=global-statement
 args = {}
 
-class update(Reader, Encoder):
-    _data_start = 0  
-    _pointer_base = 0
-    _data_stop = 0
-    _seeker = 0
-    _next_data = 0
-    _decode_bytes = bytearray()
-    _pointer_test = []
-    _pointer_base = 0
-
-    def __init__(
-        self, database: Union[AnyStr, int, PathLike, IO], mode: int = MODE_AUTO
-    ) -> None:
-        Reader.__init__(self, database, mode)
-        self._data_start = self.data_section_start
-        self._seeker = self.data_section_start
-        self._data_stop = self.data_section_stop
-        self._next_data = self.next_pointer
-        self._pointer_base = self._data_start
-        self._encode = Encoder(cache=False)
-        #self._encode = Encoder()
-        self._filename = database
-        shutil.copyfile(database, database+".trim")
-        self._fh = open(database+".trim", 'r+b')
-        self.data_cache = {}
-
-    @property
-    def data_section_start(self) -> int:
-        metadata = self.metadata()
-        treesize = int(((metadata.record_size * 2)/8) * metadata.node_count)
-        _ptr = treesize + 16
-        return _ptr 
-
-    @property
-    def data_section_stop(self) -> int:
-        return self._buffer.rfind(
-                self._METADATA_START_MARKER, max(0, self._buffer_size - 128 * 1024)
-        )
-    
-    @property
-    def seeker(self) -> int:
-        return self._seeker
-    
-    @property
-    def next_pointer(self) -> int:
-        (_, self._next_data) = self.decode(self._seeker)
-        return self._next_data
-    
-    @property
-    def show_data(self):
-        (content, _) = self.decode(self._seeker)
-        return content
-
-    @property
-    def next_data(self) -> int:
-        self._seeker = self._next_data
-        self.next_pointer
-        (content, _) = self.decode(self._seeker)
-        if self.next_pointer == self._data_stop:
-            self._seeker = self._data_start
-            self._next_data = self._data_start
-        return content
-
-    def decode(self, offset: int) -> Tuple[Record, int]:
-        return self._decoder.decode(offset)
-
-    def encode(self, value, type_id=None):
-        res = self._encode.encode(value)
-        print (self._encode.data_cache)
-        return res
-
-    def extract(self, start_marker: int, end_marker: int) -> list:
-        if end_marker == 0:
-            return self._buffer[start_marker:]
-        else:
-            return self._buffer[start_marker:end_marker]
-
-def decode_byte(byte_pointer: bytes, reader):
-    # check what type of pointer by extracting first first of the input
-    mask = int('00011000', 2) 
-    mask_pointer = int('00000111', 2)
-    cp = byte_pointer[0] & mask
-    
-    print ("byte pointer: ",byte_pointer.hex(), "CP: ",cp)
-
-    if cp == 0:
-        res = ((byte_pointer[0] & mask_pointer) << 8) | byte_pointer[1]
-    if cp == 1:
-        res = ((byte_pointer[0] & mask_pointer) << 16) | (byte_pointer[1] << 8) | byte_pointer[2] + 2048,
-    if cp == 2:
-        res =((byte_pointer[0] & mask_pointer) << 24) | (byte_pointer[1] << 16) | (byte_pointer[2] << 8) | byte_pointer[3]  + 526336,
-    if cp == 3:
-        res = (byte_pointer[1] << 24) | (byte_pointer[2] << 16) | (byte_pointer[3] << 8) | byte_pointer[4],
-
-    return struct.pack('>I', res + reader._metadata.node_count + 16)
-
 
 def decode_pointer(res, reader):
     if len(res) == 5:
-        _, pointer = struct.unpack('>BI', res)
+        _, pointer = struct.unpack(">BI", res)
     elif len(res) == 4:
-        b0, b1, b2, b3 = struct.unpack('>BBBB', res)
+        b0, b1, b2, b3 = struct.unpack(">BBBB", res)
         pointer = (b0 & 0x07) << 24 | b1 << 16 | b2 << 8 | b3
         pointer += 526336
     elif len(res) == 3:
-        b0, b1, b2 = struct.unpack('>BBB', res)
+        b0, b1, b2 = struct.unpack(">BBB", res)
         pointer = (b0 & 0x07) << 16 | b1 << 8 | b2
         pointer += 2048
     elif len(res) == 2:
-        b0, b1 = struct.unpack('>BB', res)
+        b0, b1 = struct.unpack(">BB", res)
         pointer = (b0 & 0x07) << 8 | b1
     else:
         raise ValueError("Invalid encoded pointer")
-    return struct.pack('>I', pointer + reader._metadata.node_count + 16)
+    return struct.pack(">I", pointer + reader._metadata.node_count + 16)
+
 
 def filter_dict(raw):
     """
     filter and remove keys from dictionary. The keys to be removed are store in ignore_keys and ignore_lang
     list
     """
-    ignore_keys = ["geoname_id","confidence","organization","accuracy_radius","time_zone","isp","domain","postal","metro_code"]
-    ignore_lang = ["de","es","fr","ja","pt-BR","ru","zh-CN"]
-    rem_keys = ignore_keys + ignore_lang 
-    fnc = lambda sub: [
-        fnc(item) if isinstance(item, dict) else item
-        for item in sub
+    ignore_keys = [
+        "geoname_id",
+        "confidence",
+        "organization",
+        "accuracy_radius",
+        "time_zone",
+        "isp",
+        "domain",
+        "postal",
+        "metro_code",
+    ]
+    ignore_lang = ["de", "es", "fr", "ja", "pt-BR", "ru", "zh-CN"]
+    rem_keys = ignore_keys + ignore_lang
+    fnc = lambda sub: (
+        [
+            fnc(item) if isinstance(item, dict) else item
+            for item in sub
+            if isinstance(sub, list)
+        ]
         if isinstance(sub, list)
-    ] if isinstance(sub, list) else {
-        key1: fnc(val1) if isinstance(val1, (dict, list)) else val1
-        for key1, val1 in sub.items()
-        if key1 not in rem_keys
-    }
+        else {
+            key1: fnc(val1) if isinstance(val1, (dict, list)) else val1
+            for key1, val1 in sub.items()
+            if key1 not in rem_keys
+        }
+    )
     return fnc(raw)
+
 
 def load_db(fname):
     """
@@ -177,27 +109,38 @@ def load_db(fname):
     """
     result = []
     mreader = maxminddb.open_database(fname)
-    res = (((prefix.compressed),filter_dict(data)) for prefix, data in mreader)
+    res = (((prefix.compressed), filter_dict(data)) for prefix, data in mreader)
     mreader.close
-    return (res)
+    return res
+
 
 def rewrite(fname, dic_data):
-    shutil.copyfile(fname, fname+".trim")
-    fh = open(fname+".trim", 'r+b')
+    shutil.copyfile(fname, fname + ".trim")
+    fh = open(fname + ".trim", "r+b")
     with Reader(fname) as reader:
         metadata = reader.metadata()
-        treesize = int(((metadata.record_size * 2)/8) * metadata.node_count)
+        treesize = int(((metadata.record_size * 2) / 8) * metadata.node_count)
         data_section_start = treesize + 16
+        data_section_end = reader._buffer.rfind(
+            reader._METADATA_START_MARKER, max(0, reader._buffer_size - 128 * 1024)
+        )
+
         resolved = data_section_start
-        data_section_end = reader._buffer.rfind(reader._METADATA_START_MARKER, max(0, reader._buffer_size - 128 * 1024))
         metadata_cache = reader._buffer[data_section_end:]
         encode_record = Encoder(cache=True)
-        for prefix,record in dic_data:
-            a = prefix.split('/')[0]
+        for prefix, record in dic_data:
+            a = prefix.split("/")[0]
             address = bytearray(ipaddress.ip_address(a).packed)
+            """
+            Encode the dictionary record using the Encode Record Object. Cache is set to
+            True. This ensure that repeated data (strings, float, integers) are referenced 
+            by pointers instead of re-encoding the same data. This save bytes. We also
+            Use the return (data pointer) from the Encode Record Object, for locating the
+            relevent search tree node. We can update the leaf node pointer to this new encoded
+            data as the data had changed.
+            """
             pack = encode_record.encode(record)
             pack_pointer = decode_pointer(pack, reader)
-            end_marker = resolved + len(encode_record.data_list)
             """
             Update the search tree leaf node with the pointer of the data for this node
             using the custom find_address_in_tree_loc function from Decoder module. The 
@@ -205,41 +148,44 @@ def rewrite(fname, dic_data):
             Result :      Tuple of location of leaf node (loc) and the pointer 
                           (data_pointer) to the data. 
             """
-            ((data_pointer, loc), prefix_length) = reader._find_address_in_tree_loc(address)
+            ((data_pointer, loc), prefix_length) = reader._find_address_in_tree_loc(
+                address
+            )
+            ptr = encode_record._encode_pointer(resolved)
+            new_pointer = decode_pointer(ptr, reader)
             fh.seek(loc)
             fh.write(pack_pointer)
             """
-            write the buf onto the file and clear the encoding data_list in the encoder
-            This will prevent overloading the memory
+            extract the data content from the Encoded Record Object. Once extract the required data
+            zero out the data list. This is to ensure the Encoded Record Object remain lean. However,
+            we need to ensure this data is written to the mmdb file.
             """
-            buff = encode_record.data_list
-            encode_record.data_list = []
-            print (b''.join([bytes_obj for bytes_obj in buff]))
+            encode_data = encode_record.data_list
+            data_bytes = b"".join([bytes_obj for bytes_obj in encode_data])
             fh.seek(resolved)
-            fh.write((b''.join([bytes_obj for bytes_obj in buff])))
-            resolved = end_marker
-
-        #pack_list = (b''.join([bytes_obj for bytes_obj in encode_record.data_list]))
-        #fh.seek(data_section_start)
-        #fh.write(pack_list)
-        fh.seek(resolved)
+            fh.write(data_bytes)
+            resolved += len(data_bytes)
+            encode_record.data_list = []
         fh.write(metadata_cache)
     fh.close()
+
 
 def main():
     """
     main function for the workflow
     """
-    parser = get_args([mmdb_arg, ipaddress_arg, asn_arg, display_arg, show_db_type_arg, trim_arg])
+    parser = get_args(
+        [mmdb_arg, ipaddress_arg, asn_arg, display_arg, show_db_type_arg, trim_arg]
+    )
     global args
     args = parser.parse_args()
     if not os.path.isfile(args.mmdb):
         parser.print_help(sys.stderr)
         sys.exit(1)
 
+
 if __name__ == "__main__":
     main()
     fname = args.mmdb
     db = load_db(fname)
     rewrite(fname, db)
-    
--- mrt2mmdb/mmdb_writer.py
+++ mrt2mmdb/mmdb_writer.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-__version__ = '0.1.1'
+__version__ = "0.1.1"
 
 import logging
 import math
@@ -13,43 +13,7 @@ MMDBType = Union[dict, list, str, bytes, int, bool]
 
 logger = logging.getLogger(__name__)
 
-METADATA_MAGIC = b'\xab\xcd\xefMaxMind.com'
-
-
-class SearchTreeNode(object):
-    def __init__(self, left=None, right=None):
-        self.left = left
-        self.right = right
-
-    def get_or_create(self, item):
-        if item == 0:
-            self.left = self.left or SearchTreeNode()
-            return self.left
-        elif item == 1:
-            self.right = self.right or SearchTreeNode()
-            return self.right
-
-    def __getitem__(self, item):
-        if item == 0:
-            return self.left
-        elif item == 1:
-            return self.right
-
-    def __setitem__(self, key, value):
-        if key == 0:
-            self.left = value
-        elif key == 1:
-            self.right = value
-
-
-class SearchTreeLeaf(object):
-    def __init__(self, value):
-        self.value = value
-
-    def __repr__(self):
-        return "SearchTreeLeaf(value={value})".format(value=self.value)
-
-    __str__ = __repr__
+METADATA_MAGIC = b"\xab\xcd\xefMaxMind.com"
 
 
 class Encoder(object):
@@ -65,24 +29,31 @@ class Encoder(object):
     def _encode_pointer(self, value):
         pointer = value
         if pointer >= 134744064:
-            res = struct.pack('>BI', 0x38, pointer)
+            res = struct.pack(">BI", 0x38, pointer)
         elif pointer >= 526336:
             pointer -= 526336
-            res = struct.pack('>BBBB', 0x30 + ((pointer >> 24) & 0x07),
-                              (pointer >> 16) & 0xff, (pointer >> 8) & 0xff,
-                              pointer & 0xff)
+            res = struct.pack(
+                ">BBBB",
+                0x30 + ((pointer >> 24) & 0x07),
+                (pointer >> 16) & 0xFF,
+                (pointer >> 8) & 0xFF,
+                pointer & 0xFF,
+            )
         elif pointer >= 2048:
             pointer -= 2048
-            res = struct.pack('>BBB', 0x28 + ((pointer >> 16) & 0x07),
-                              (pointer >> 8) & 0xff, pointer & 0xff)
+            res = struct.pack(
+                ">BBB",
+                0x28 + ((pointer >> 16) & 0x07),
+                (pointer >> 8) & 0xFF,
+                pointer & 0xFF,
+            )
         else:
-            res = struct.pack('>BB', 0x20 + ((pointer >> 8) & 0x07),
-                              pointer & 0xff)
+            res = struct.pack(">BB", 0x20 + ((pointer >> 8) & 0x07), pointer & 0xFF)
 
         return res
 
     def _encode_utf8_string(self, value):
-        encoded_value = value.encode('utf-8')
+        encoded_value = value.encode("utf-8")
         res = self._make_header(2, len(encoded_value))
         res += encoded_value
         return res
@@ -92,9 +63,9 @@ class Encoder(object):
 
     def _encode_uint(self, type_id, max_len):
         def _encode_unsigned_value(value):
-            res = b''
+            res = b""
             while value != 0 and len(res) < max_len:
-                res = struct.pack('>B', value & 0xff) + res
+                res = struct.pack(">B", value & 0xFF) + res
                 value = value >> 8
             return self._make_header(type_id, len(res)) + res
 
@@ -132,63 +103,56 @@ class Encoder(object):
             self._type_decoder = {
                 1: self._encode_pointer,
                 2: self._encode_utf8_string,
-                3: self._encode_pack_type(3, '>d'),  # double,
+                3: self._encode_pack_type(3, ">d"),  # double,
                 4: self._encode_bytes,
                 5: self._encode_uint(5, 2),  # uint16
                 6: self._encode_uint(6, 4),  # uint32
                 7: self._encode_map,
-                8: self._encode_pack_type(8, '>i'),  # int32
+                8: self._encode_pack_type(8, ">i"),  # int32
                 9: self._encode_uint(9, 8),  # uint64
                 10: self._encode_uint(10, 16),  # uint128
                 11: self._encode_array,
                 14: self._encode_boolean,
-                15: self._encode_pack_type(15, '>f'),  # float,
+                15: self._encode_pack_type(15, ">f"),  # float,
             }
         return self._type_decoder
 
     def _make_header(self, type_id, length):
         if length >= 16843036:
-            raise Exception('length >= 16843036')
+            raise Exception("length >= 16843036")
 
         elif length >= 65821:
             five_bits = 31
             length -= 65821
-            b3 = length & 0xff
-            b2 = (length >> 8) & 0xff
-            b1 = (length >> 16) & 0xff
-            additional_length_bytes = struct.pack('>BBB', b1, b2, b3)
+            b3 = length & 0xFF
+            b2 = (length >> 8) & 0xFF
+            b1 = (length >> 16) & 0xFF
+            additional_length_bytes = struct.pack(">BBB", b1, b2, b3)
 
         elif length >= 285:
             five_bits = 30
             length -= 285
-            b2 = length & 0xff
-            b1 = (length >> 8) & 0xff
-            additional_length_bytes = struct.pack('>BB', b1, b2)
+            b2 = length & 0xFF
+            b1 = (length >> 8) & 0xFF
+            additional_length_bytes = struct.pack(">BB", b1, b2)
 
         elif length >= 29:
             five_bits = 29
             length -= 29
-            additional_length_bytes = struct.pack('>B', length & 0xff)
+            additional_length_bytes = struct.pack(">B", length & 0xFF)
 
         else:
             five_bits = length
-            additional_length_bytes = b''
+            additional_length_bytes = b""
 
         if type_id <= 7:
-            res = struct.pack('>B', (type_id << 5) + five_bits)
+            res = struct.pack(">B", (type_id << 5) + five_bits)
         else:
-            res = struct.pack('>BB', five_bits, type_id - 7)
+            res = struct.pack(">BB", five_bits, type_id - 7)
 
         return res + additional_length_bytes
 
-    _python_type_id = {
-        float: 15,
-        bool: 14,
-        list: 11,
-        dict: 7,
-        bytes: 4,
-        str: 2
-    }
+    _python_type_id = {float: 15, bool: 14, list: 11, dict: 7, bytes: 4, str: 2}
 
     def python_type_id(self, value):
         value_type = type(value)
@@ -196,11 +160,11 @@ class Encoder(object):
         if type_id:
             return type_id
         if value_type is int:
-            if value > 0xffffffffffffffff:
+            if value > 0xFFFFFFFFFFFFFFFF:
                 return 10
-            elif value > 0xffffffff:
+            elif value > 0xFFFFFFFF:
                 return 9
-            elif value > 0xffff:
+            elif value > 0xFFFF:
                 return 6
             elif value < 0:
                 return 8
@@ -210,9 +174,14 @@ class Encoder(object):
 
     def encode_meta(self, meta):
         res = self._make_header(7, len(meta))
-        meta_type = {'node_count': 6, 'record_size': 5, 'ip_version': 5,
-                     'binary_format_major_version': 5, 'binary_format_minor_version': 5,
-                     'build_epoch': 9}
+        meta_type = {
+            "node_count": 6,
+            "record_size": 5,
+            "ip_version": 5,
+            "binary_format_major_version": 5,
+            "binary_format_minor_version": 5,
+            "build_epoch": 9,
+        }
         for k, v in list(meta.items()):
             # Keys are always stored by value.
             res += self.encode(k)
@@ -222,7 +191,7 @@ class Encoder(object):
     def encode(self, value, type_id=None):
         if self.cache:
             try:
-                if not isinstance(value, (list,dict)):
+                if not isinstance(value, (list, dict)):
                     return self.data_cache[value]
             except KeyError:
                 pass
@@ -247,217 +216,11 @@ class Encoder(object):
                 pointer_position = self.data_pointer
                 self.data_pointer += len(res)
                 pointer = self.encode(pointer_position, 1)
-                if not isinstance(value, (list,dict)):
+                if not isinstance(value, (list, dict)):
                     self.data_cache[value] = pointer
                 return pointer
         return res
 
 
-class TreeWriter(object):
-    encoder_cls = Encoder
-
-    def __init__(self, tree, meta):
-        self._node_idx = {}
-        self._leaf_offset = {}
-        self._node_list = []
-        self._node_counter = 0
-        self._record_size = 0
-
-        self.tree = tree
-        self.meta = meta
-
-        self.encoder = self.encoder_cls(cache=True)
-
-    @property
-    def _data_list(self):
-        return self.encoder.data_list
-
-    @property
-    def _data_pointer(self):
-        return self.encoder.data_pointer + 16
-
-    def _build_meta(self):
-        return {
-            "node_count": self._node_counter,
-            "record_size": self.record_size,
-            **self.meta
-        }
-
-    def _adjust_record_size(self):
-        # Tree records should be large enough to contain either tree node index
-        # or data offset.
-        max_id = self._node_counter + self._data_pointer + 1
-
-        # Estimate required bit count.
-        bit_count = int(math.ceil(math.log(max_id, 2)))
-        if bit_count <= 24:
-            self.record_size = 24
-        elif bit_count <= 28:
-            self.record_size = 28
-        elif bit_count <= 32:
-            self.record_size = 32
-        else:
-            raise Exception('record_size > 32')
-
-        self.data_offset = self.record_size * 2 / 8 * self._node_counter
-
-    def _enumerate_nodes(self, node):
-        if type(node) is SearchTreeNode:
-            node_id = id(node)
-            if node_id not in self._node_idx:
-                self._node_idx[node_id] = self._node_counter
-                self._node_counter += 1
-                self._node_list.append(node)
-
-            self._enumerate_nodes(node.left)
-            self._enumerate_nodes(node.right)
-
-        elif type(node) is SearchTreeLeaf:
-            node_id = id(node)
-            if node_id not in self._leaf_offset:
-                res = self.encoder.encode(node.value)
-                self._leaf_offset[node_id] = self._data_pointer - len(res)
-        else:  # == None
-            return
-
-    def _calc_record_idx(self, node):
-        if node is None:
-            return self._node_counter
-        elif type(node) is SearchTreeNode:
-            return self._node_idx[id(node)]
-        elif type(node) is SearchTreeLeaf:
-            return self._leaf_offset[id(node)] + self._node_counter
-        else:
-            raise Exception("unexpected type")
-
-    def _cal_node_bytes(self, node) -> bytes:
-        left_idx = self._calc_record_idx(node.left)
-        right_idx = self._calc_record_idx(node.right)
-
-        if self.record_size == 24:
-            b1 = (left_idx >> 16) & 0xff
-            b2 = (left_idx >> 8) & 0xff
-            b3 = left_idx & 0xff
-            b4 = (right_idx >> 16) & 0xff
-            b5 = (right_idx >> 8) & 0xff
-            b6 = right_idx & 0xff
-            return struct.pack('>BBBBBB', b1, b2, b3, b4, b5, b6)
-
-        elif self.record_size == 28:
-            b1 = (left_idx >> 16) & 0xff
-            b2 = (left_idx >> 8) & 0xff
-            b3 = left_idx & 0xff
-            b4 = ((left_idx >> 24) & 0xf) * 16 + \
-                 ((right_idx >> 24) & 0xf)
-            b5 = (right_idx >> 16) & 0xff
-            b6 = (right_idx >> 8) & 0xff
-            b7 = right_idx & 0xff
-            return struct.pack('>BBBBBBB', b1, b2, b3, b4, b5, b6, b7)
-
-        elif self.record_size == 32:
-            return struct.pack('>II', left_idx, right_idx)
-
-        else:
-            raise Exception('self.record_size > 32')
-
-    def write(self, fname):
-        self._enumerate_nodes(self.tree)
-        self._adjust_record_size()
-
-        with open(fname, 'wb') as f:
-            for node in self._node_list:
-                f.write(self._cal_node_bytes(node))
-
-            f.write(b'\x00' * 16)
-
-            for element in self._data_list:
-                f.write(element)
-
-            f.write(METADATA_MAGIC)
-            f.write(self.encoder_cls(cache=False).encode_meta(self._build_meta()))
-
-
 def bits_rstrip(n, length=None, keep=0):
-    return map(int, bin(n)[2:].rjust(length, '0')[:keep])
-
-
-class MMDBWriter(object):
-
-    def __init__(self, ip_version=4, database_type='GeoIP',
-                 languages=None, description='GeoIP db',
-                 ipv4_compatible=False):
-        self.tree = SearchTreeNode()
-        self.ipv4_compatible = ipv4_compatible
-
-        if languages is None:
-            languages = []
-        self.description = description
-        self.database_type = database_type
-        self.ip_version = ip_version
-        self.languages = languages
-        self.binary_format_major_version = 2
-        self.binary_format_minor_version = 0
-
-        self._bit_length = 128 if ip_version == 6 else 32
-
-        if ip_version not in [4, 6]:
-            raise ValueError("ip_version should be 4 or 6, {} is incorrect".format(ip_version))
-        if ip_version == 4 and ipv4_compatible:
-            raise ValueError("ipv4_compatible=True can set when ip_version=6")
-        if not self.binary_format_major_version:
-            raise ValueError("major_version can't be empty or 0: {}".format(self.binary_format_major_version))
-        if isinstance(description, str):
-            self.description = {i: description for i in languages}
-        for i in languages:
-            if i not in self.description:
-                raise ValueError("language {} must have description!")
-
-    def insert_network(self, network: IPSet, content: MMDBType):
-        leaf = SearchTreeLeaf(content)
-        if not isinstance(network, IPSet):
-            raise ValueError("network type should be netaddr.IPSet.")
-        network = network.iter_cidrs()
-        for cidr in network:
-            if self.ip_version == 4 and cidr.version == 6:
-                raise ValueError('You inserted a IPv6 address {} '
-                                 'to an IPv4-only database.'.format(cidr))
-            if self.ip_version == 6 and cidr.version == 4:
-                if not self.ipv4_compatible:
-                    raise ValueError("You inserted a IPv4 address {} to an IPv6 database."
-                                     "Please use ipv4_compatible=True option store "
-                                     "IPv4 address in IPv6 database as ::/96 format".format(cidr))
-                cidr = cidr.ipv6(True)
-            node = self.tree
-            bits = list(bits_rstrip(cidr.value, self._bit_length, cidr.prefixlen))
-            current_node = node
-            supernet_leaf = None  # Tracks whether we are inserting into a subnet
-            for (index, ip_bit) in enumerate(bits[:-1]):
-                previous_node = current_node
-                current_node = previous_node.get_or_create(ip_bit)
-
-                if isinstance(current_node, SearchTreeLeaf):
-                    current_cidr = IPNetwork((int(''.join(map(str, bits[:index + 1])).ljust(self._bit_length, '0'), 2), index + 1))
-                    logger.info(f"Inserting {cidr} ({content}) into subnet of {current_cidr} ({current_node.value})")
-                    supernet_leaf = current_node
-                    current_node = SearchTreeNode()
-                    previous_node[ip_bit] = current_node
-
-                if supernet_leaf:
-                    next_bit = bits[index + 1]
-                    # Insert supernet information on each inverse bit of the current subnet
-                    current_node[1 - next_bit] = supernet_leaf
-            current_node[bits[-1]] = leaf
-
-    def to_db_file(self, filename: str):
-        return TreeWriter(self.tree, self._build_meta()).write(filename)
-
-    def _build_meta(self):
-        return {
-            "ip_version": self.ip_version,
-            "database_type": self.database_type,
-            "languages": self.languages,
-            "binary_format_major_version": self.binary_format_major_version,
-            "binary_format_minor_version": self.binary_format_minor_version,
-            "build_epoch": int(time.time()),
-            "description": self.description,
-        }
+    return map(int, bin(n)[2:].rjust(length, "0")[:keep])

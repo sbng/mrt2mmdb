c5a92b435b95a75d04f4bc546f4d64191b7f63b3
sbng <seo.boon.ng@gmail.com>
2024-09-16 16:36:07 +0800

    Write test cases for filter.py using shell script assert.sh

---
+++ tests/test_filter.py
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+source ./assert.sh
+
+# make mmdb file using input json and run filter.py to trim the mmdb
+./mmdbctl import -f json small.json small.mmdb >/dev/null 2>&1
+../mrt2mmdb/filter.py --mmdb small.mmdb --trim `cat ./ignore.list` --quiet
+assert_raises "true"
+
+# make mmdb file using input json and run filter.py to trim the mmdb
+./mmdbctl import -f json medium.json medium.mmdb >/dev/null 2>&1
+../mrt2mmdb/filter.py --mmdb medium.mmdb --trim `cat ./ignore.list` --quiet
+assert_raises "true"
+
+verify_small=$(./mmdbctl export -f json small.mmdb | jq -r '.network,.location.latitude,.location.longitude')
+verify_medium=$(./mmdbctl export -f json medium.mmdb | jq -r '.network,.location.latitude,.location.longitude')
+
+
+alias exec_small="./mmdbctl export -f json small.mmdb.trim | jq -r '.network,.location.latitude,.location.longitude'" 
+alias exec_medium="./mmdbctl export -f json medium.mmdb.trim | jq -r '.network,.location.latitude,.location.longitude'" 
+
+# Verify the mmdb.trim vs mmdb file
+assert "exec_small" "$verify_small"
+assert "exec_medium" "$verify_medium"
+assert_end filter.py
